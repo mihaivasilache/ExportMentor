2376b9d74a5be3ef0472a7cc7216afde8629489b
mihaivasilache <mihai.vasilache12@gmail.com>
2018-11-27 19:13:02 +0200

    working update v2

--- bin/update.py
+++ bin/update.py
@@ -12,7 +12,7 @@ def check_for_update(update_file_name, update_file=False):
     response = requests.get("https://raw.githubusercontent.com/mihaivasilache/ExportMentor/master/%s"
                             % update_file_name)
     text_form_site = response.text
-    text_from_file = open(os.path.join('../', update_file_name), 'r').read()
+    text_from_file = open(os.path.join(update_file_name), 'r').read()
     if text_form_site != text_from_file:
         fd = open(os.path.join(update_file_name), 'w')
         fd.write(text_form_site)
